bed44085bdc4961b89bb069eda49c06def8a370f
Luisa <luisafranconeves@gmail.com>
2022-03-18 11:46:40 -0300

    ...

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,13 @@
-print("ola")
+print("********************************")
+print("Bem vindo no jogo de Adivinhação")
+print("********************************")
+
+numero_secreto = 42
+
+chute = input("Digite o seu número: ")
+print("Você digitou ", chute)
+
+if numero_secreto == chute:
+    print("Você acertou!!")
+else:
+    print("Você errou.")
