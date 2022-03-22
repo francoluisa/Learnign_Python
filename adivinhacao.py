6383426111242da49b132c10b3cee55d62083b03
Luisa <luisafranconeves@gmail.com>
2022-03-22 12:02:36 -0300

    * break * continue * and * or

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,11 +5,16 @@ print("********************************")
 numero_secreto = 42
 total_de_tentativas = 3
 
+
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}". format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     chute_maior = chute > numero_secreto
@@ -17,6 +22,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou:
         print("Você acertou!!")
+        break
     else:
         if chute_maior:
             print("O seu chute foi maior que o número secreto.")
