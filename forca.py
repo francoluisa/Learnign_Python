3fa3881af65157d8fea3ced6d0c38aed9801c8f4
Luisa <luisafranconeves@gmail.com>
2022-03-30 12:23:05 -0300

    addition of a file to choose games

--- forca.py
+++ forca.py
@@ -1,11 +1,25 @@
-
 def jogar():
     print("********************************")
     print("Bem vindo no jogo de Forca")
     print("********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print(f"Encontrei a letra {letra} na posição {index}.")
+            index = index + 1
+        print("Jogando ...")
+
     print("Fim de jogo!")
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     jogar()
