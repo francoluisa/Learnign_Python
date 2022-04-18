3649bc72cf99c6b6ae8deca781a60dff38653baa
Luisa <luisafranconeves@gmail.com>
2022-04-18 13:23:53 -0300

    .

--- forca.py
+++ forca.py
@@ -3,20 +3,37 @@ def jogar():
     print("Bem vindo no jogo de Forca")
     print("********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while not enforcou and not acertou:
 
         chute = input("Qual a letra? ")
-        chute = chute.strip()
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                print(f"Encontrei a letra {letra} na posição {index}.")
-            index = index + 1
-        print("Jogando ...")
+        chute = chute.strip().upper()
+
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu.")
 
     print("Fim de jogo!")
 
---
+++ madlibs.py
@@ -0,0 +1,11 @@
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programing is so {adj}! It makes me so excited all the time because  " \
+             f"i love to {verb1}. Stay hydrated and {verb2} like you're {famous_person}!"
+
+print(madlib)
